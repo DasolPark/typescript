964d9b418493e36981102c2744c3555f60cc97c4
Dasol Park <dasol.park615@gmail.com>
2021-03-27 23:08:00 +0900

    Type: function optional, default and rest parameter

--- 1_types/1-2_function.ts
+++ 1_types/1-2_function.ts
@@ -1,31 +1,58 @@
 {
-  // JavaScript 💩
-  function jsAdd(num1, num2) {
-    return num1 + num2;
+  // // JavaScript 💩
+  // function jsAdd(num1, num2) {
+  //   return num1 + num2;
+  // }
+
+  // // TypeScript ✨
+  // function add(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
+
+  // // JavaScript 💩
+  // function jsFetchNum(id) {
+  //   // code ...
+  //   // code ...
+  //   // code ...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  // // TypeScript ✨
+  // function fetchNum(id: string): Promise<number> {
+  //   // code ...
+  //   // code ...
+  //   // code ...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  // JavaScript ✨ => TypeScript
+  // Optional parameter
+  function printName(firstName: string, lastName?: string) {
+    console.log(firstName);
+    console.log(lastName);
   }
 
-  // TypeScript ✨
-  function add(num1: number, num2: number): number {
-    return num1 + num2;
+  printName('David', 'park');
+  printName('Peter');
+  printName('Anna', undefined);
+
+  // Default parameter
+  function printMessage(message: string = 'default message') {
+    console.log(message);
+  }
+  printMessage('hi david');
+  printMessage();
+
+  // Rest parameter
+  function addNumbers(...numbers: number[]): number {
+    return numbers.reduce((acc, num) => acc += num, 0);
   }
 
-  // JavaScript 💩
-  function jsFetchNum(id) {
-    // code ...
-    // code ...
-    // code ...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
-  }
-
-  // TypeScript ✨
-  function fetchNum(id: string): Promise<number> {
-    // code ...
-    // code ...
-    // code ...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
-  }
+  console.log(addNumbers(1, 2));
+  console.log(addNumbers(1, 2, 3, 4));
+  console.log(addNumbers(1, 2, 3, 4, 5, 0));
 }
--- README.md
+++ README.md
@@ -16,3 +16,13 @@ npm install -g ts-node
 - never는 error handling의 경우 error message를 서버에 보내고, throw Error('message')를 통해 끝낼 때 사용. 즉, return;까지 도달하지 않을 때 사용
 #### object
 - primitive를 제외한 모든 type이 object이므로 단순히 { key: value } 를 사용한다고 object type를 사용할 수 없음
+
+### function
+#### 문서화 효과를 볼 수 있음
+
+#### optional parameter(TS feature)
+- 선택적으로 파라미터를 넘길 수 있음(e.g. ``param?: string == string | undefined``)
+- 하지만, ``param: string | undefined``는 위와 다름. 해당 파라미터 인풋을 생략할 수 없음
+#### default parameter
+#### rest parameter
+- ``...args``를 이용해 어떤 수의 parameter도 array로 받을 수 있음
