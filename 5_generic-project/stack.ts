ba1b220125678c47cb150bb1ef53ebc7e0371f44
Dasol Park <dasol.park615@gmail.com>
2021-04-13 22:45:05 +0900

    Generic: Challenge making STACK

---
+++ 5_generic-project/stack.ts
@@ -0,0 +1,51 @@
+{
+  interface Stack<T> {
+    readonly size: number;
+    push(value: T): void;
+    pop(): T;
+  }
+
+  type StackNode<T> = {
+    value: T;
+    next?: StackNode<T>;
+  }
+
+  class StackImpl<T> implements Stack<T> {
+    private _size: number = 0;
+    private head?: StackNode<T>;
+
+    constructor(private capacity: number) { }
+
+    get size() {
+      return this._size;
+    }
+
+    push(value: T) {
+      if (this.size >= this.capacity) {
+        throw new Error('full stack!');
+      }
+
+      const node = { value, next: this.head };
+      this.head = node;
+      this._size++;
+    }
+
+    pop(): T {
+      if (this.head == null) {
+        throw new Error('empty stack!');
+      }
+      const node = this.head;
+      this.head = node?.next;
+      return node?.value;
+    }
+  }
+
+  const stack = new StackImpl<string>(30);
+  stack.push('b');
+  stack.push('a');
+  stack.push('c');
+
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
+}
--- README.md
+++ README.md
@@ -144,3 +144,6 @@ npm install -g ts-node
 - 따라서, 해당 타입을 잃지 않도록 정확히 타입을 지정해주는 것이 좋다
 - e.g. function pay<T extends Employee>(employee: T):T {} Employee interface를 확장한 Type만 가능하다는 뜻
 - e.g. function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {} 해당 object에 있는 key만 가능하다는 뜻
+
+## Generic Challenge(STACK)
+
