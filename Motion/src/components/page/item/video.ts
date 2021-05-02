9e71bd67dc6047edbced0ae338e4d0e99b300ee4
Dasol Park <dasol.park615@gmail.com>
2021-05-02 09:31:53 +0900

    Motion: Video applied yet regex

--- Motion/src/components/page/item/video.ts
+++ Motion/src/components/page/item/video.ts
@@ -1,24 +1,31 @@
-// import { BaseComponent } from "../../component";
+import { BaseComponent } from "../../component.js";
 
-// export class VideoComponent extends BaseComponent<HTMLElement> {
-//   constructor(title: string, url: string) {
-//     super(`
-//       <section class="video">
-//         <div class="video__holder">
-//           <iframe class="video__content">
-//           </iframe>
-//         </div>
-//         <p class="video__title"></p>
-//       </section>
-//     `);
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`
+      <section class="video">
+        <div class="video__player">
+          <iframe class="video__iframe"></iframe>
+        </div>
+        <h3 class="video__title"></h3>
+      </section>
+    `);
 
-//     const videoElement = this.element.querySelector('.video__content')! as HTMLIFrameElement;
-//     videoElement.src = url;
-//     videoElement.title = title;
+    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
+    console.log(url);
+    iframe.src = 'https://www.youtube.com/embed/I5DJrMeR228'; // url -> videoId
 
-//     const titleElement = this.element.querySelector('.video__title')! as HTMLParagraphElement;
-//     titleElement.textContent = title;
-//   }
-// }
+    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+}
 
-/** written by David */
+// <iframe 
+//   width="560"
+//   height = "315"
+//   src = "https://www.youtube.com/embed/I5DJrMeR228"
+//   title = "YouTube video player"
+//   frameborder = "0"
+//   allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//   allowfullscreen
+// > </iframe>
