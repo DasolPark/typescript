d34342cdbeedd3b5735aa007f9844705c809d0de
Dasol Park <dasol.park615@gmail.com>
2021-05-02 09:17:54 +0900

    Motion: Note & Todo

--- Motion/src/components/page/item/image.ts
+++ Motion/src/components/page/item/image.ts
@@ -1,4 +1,4 @@
-import { BaseComponent } from "../component.js";
+import { BaseComponent } from "../../component.js";
 
 export class ImageComponent extends BaseComponent<HTMLElement>{
   constructor(title: string, url: string) {
@@ -7,7 +7,7 @@ export class ImageComponent extends BaseComponent<HTMLElement>{
         <div class="image__holder">
           <img class="image__thumbnail">
         </div>
-        <p class="image__title"></p>
+        <h2 class="image__title"></h2>
       </section>
     `);
 
@@ -15,7 +15,7 @@ export class ImageComponent extends BaseComponent<HTMLElement>{
     imageElement.src = url;
     imageElement.alt = title;
 
-    const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+    const titleElement = this.element.querySelector('.image__title')! as HTMLHeadingElement;
     titleElement.textContent = title;
   }
 }
---
+++ Motion/src/components/page/item/video.ts
@@ -0,0 +1,24 @@
+// import { BaseComponent } from "../../component";
+
+// export class VideoComponent extends BaseComponent<HTMLElement> {
+//   constructor(title: string, url: string) {
+//     super(`
+//       <section class="video">
+//         <div class="video__holder">
+//           <iframe class="video__content">
+//           </iframe>
+//         </div>
+//         <p class="video__title"></p>
+//       </section>
+//     `);
+
+//     const videoElement = this.element.querySelector('.video__content')! as HTMLIFrameElement;
+//     videoElement.src = url;
+//     videoElement.title = title;
+
+//     const titleElement = this.element.querySelector('.video__title')! as HTMLParagraphElement;
+//     titleElement.textContent = title;
+//   }
+// }
+
+/** written by David */
